4cd5556a33580a2b10357538b32f6a5210e8da93
Huỳnh Tấn Tiến <tantien3003@gmail.com>
2026-05-07 13:58:41 +0700

    Updated

--- .history/include/job_20260507133754.h
+++ .history/include/job_20260507133754.h
@@ -1,14 +1,14 @@
 #ifndef JOB_H
 #define JOB_H
 
-// Define the states of a background job
+// Định nghĩa các trạng thái của job [cite: 95-99]
 typedef enum {
     JOB_WAITING,
     JOB_RUNNING,
     JOB_DONE
 } job_status_t;
 
-// Structure to store information about a job
+// Cấu trúc lưu trữ thông tin một job [cite: 100-110]
 typedef struct {
     int job_id;
     char seller_id[32];
